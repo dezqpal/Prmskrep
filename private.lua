6b07d3e1be3545887bf505c235b53c14c6addad5
dezqpal <dezqpal@gmail.com>
2026-02-13 14:01:10 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- private.lua
+++ private.lua
@@ -1,6 +1,6 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/memejames/elerium-v2-ui-library//main/Library", true))()
 
-local window = library:AddWindow("Primo Private Script", {
+local window = library:AddWindow("PRIMO |•PRIVATE SCRIPT ", {
     main_color = Color3.fromRGB(0, 0, 0),
     min_size = Vector2.new(650, 820),
 })
@@ -2120,6 +2120,7 @@ Credits:AddLabel("Kaino").TextSize = 20
 Credits:AddLabel("UnoxDos").TextSize = 20
 
 Credits:AddLabel("🐝SLH CLAN🐝").TextSize = 20
+
 
 
 
