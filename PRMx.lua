1a2495e64e4ef65d56a0088fb6dc00aec5647b2d
PrimoScripts <dezqpal@gmail.com>
2026-02-15 11:08:00 +0800

    Update PRMx.lua

--- PRMx.lua
+++ PRMx.lua
@@ -1310,51 +1310,6 @@ PetsTab:AddSwitch("Auto Evolve Pet", function(state)
     if state then task.spawn(autoEvolveLoop) end
 end)
 
-local selectedAura = "Blue Aura"
-local auraDropdown = pets:AddDropdown("Select Aura", function(text)
-    selectedAura = text
-end)
-
-auraDropdown:Add("Astral Electro")
-auraDropdown:Add("Azure Tundra")
-auraDropdown:Add("Blue Aura")
-auraDropdown:Add("Dark Electro")
-auraDropdown:Add("Dark Lightning")
-auraDropdown:Add("Dark Storm")
-auraDropdown:Add("Electro")
-auraDropdown:Add("Enchanted Mirage")
-auraDropdown:Add("Entropic Blast")
-auraDropdown:Add("Eternal Megastrike")
-auraDropdown:Add("Grand Supernova")
-auraDropdown:Add("Green Aura")
-auraDropdown:Add("Inferno")
-auraDropdown:Add("Lightning")
-auraDropdown:Add("Muscle King")
-auraDropdown:Add("Power Lightning")
-auraDropdown:Add("Purple Aura")
-auraDropdown:Add("Purple Nova")
-auraDropdown:Add("Red Aura")
-auraDropdown:Add("Supernova")
-auraDropdown:Add("Ultra Inferno")
-auraDropdown:Add("Ultra Mirage")
-auraDropdown:Add("Unstable Mirage")
-auraDropdown:Add("Yellow Aura")
-
-pets:AddSwitch("Auto Open Aura", function(bool)
-    _G.AutoHatchAura = bool
-    if bool then
-        spawn(function()
-            while _G.AutoHatchAura and selectedAura ~= "" do
-                local auraToOpen = ReplicatedStorage.cPetShopFolder:FindFirstChild(selectedAura)
-                if auraToOpen then
-                    ReplicatedStorage.cPetShopRemote:InvokeServer(auraToOpen)
-                end
-                task.wait(0.1)
-            end
-        end)
-    end
-end)
-
 PetsTab:AddLabel("Other").TextSize = 23
 
 local playerDropdown = PetsTab:AddDropdown("Select Player", function(playerName)
