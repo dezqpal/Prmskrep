24583a2d3aaaf40efbf2acc36c6679de034d20ce
PrimoScripts <dezqpal@gmail.com>
2026-02-16 19:47:55 +0800

    Update PRMx.lua

--- PRMx.lua
+++ PRMx.lua
@@ -1,6 +1,6 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/memejames/elerium-v2-ui-library//main/Library", true))()
 
-local window = library:AddWindow("STANLEY BAKLA HAHAHAHAHAHAH", {
+local window = library:AddWindow("P.         R.         I.         M.         O.", {
     main_color = Color3.fromRGB(0, 0, 0),
     min_size = Vector2.new(650, 820),
 })
