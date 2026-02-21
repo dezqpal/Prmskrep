802638d4f963cf7f06b8713c31df13d00b77ec28
PrimoScripts <dezqpal@gmail.com>
2026-02-21 12:01:11 +0800

    Update PRMx.lua

--- PRMx.lua
+++ PRMx.lua
@@ -1396,3 +1396,25 @@ end)
 local Credits = window:AddTab("Credits")
 
 Credits:AddLabel("•MADE BY PRIMO•").TextSize = 30
+
+Credits:AddLabel("•--- My Social Media ---•")
+Credits:AddLabel(" Follow my TikTok account")
+
+Credits:AddButton("Copy TikTok Link", function()
+    setclipboard("https://www.tiktok.com/@primo_legitaccount").TextSize = 35
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "TikTok Link",
+        Text = "Link copied to clipboard!",
+        Duration = 3
+    })
+end)
+Credits:AddLabel("Dm me here for more free script")
+
+Credits:AddButton("Copy Facebook Link", function()
+    setclipboard("https://www.facebook.com/profile.php?id=61576466431107").TextSize = 35
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Facebook Link",
+        Text = "Link copied to clipboard!",
+        Duration = 3
+    })
+end)
