08221fecfaa43e5b6aae5c3ccad951c0ba88d7ec
PrimoScripts <dezqpal@gmail.com>
2026-02-22 11:01:43 +0800

    Update V2PaidW.lua

--- V2PaidW.lua
+++ V2PaidW.lua
@@ -2,7 +2,7 @@ local webhook = "https://discord.com/api/webhooks/1474909344918405120/rm53gdqtdc
 
 -- SIGURADUHIN NA TAMA ANG SPELLING AT UPPERCASE/LOWERCASE
 local whitelist = {
-    "", 
+    "iggy123982", 
     "",
     "ingalla_09",
     "YUO_WEAK",
