718c13f054e33bd26e596d47708f58a5f21ffa8e
yumehatdog <dezqpal@gmail.com>
2026-05-03 22:25:44 +0800

    Add unokeySystem.lua file

--- unokeySystem.lua
+++ unokeySystem.lua
@@ -83,7 +83,7 @@ SubmitBtn.MouseButton1Click:Connect(function()
         ScreenGui:Destroy()
         
         print("Key correct! Loading script...")
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/dezqpal/Primo/refs/heads/main/private.lua "))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/dezqpal/Prmskrep/refs/heads/main/UnObns.lua "))()
     else
         -- Error Animation
         SubmitBtn.Text = "INVALID KEY!"
